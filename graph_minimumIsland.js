1f55f3f0166e983fd60810a3e92168a6b284b10e
JCarlos <jcarlostorres20@gmail.com>
2022-01-26 16:50:41 -0600

    Minimum Island

---
+++ graph_minimumIsland.js
@@ -0,0 +1,44 @@
+const grid = [
+    ['W','L','W','W','W'],
+    ['W','L','W','W','W'],
+    ['W','W','W','L','W'],
+    ['W','W','L','L','W'],
+    ['L','w','W','L','L'],
+    ['L','L','W','W','W'],
+]
+
+const minimumIsland = (grid) => {
+    let visited = new Set();
+    let minSize = Infinity;
+    for(let r = 0; r < grid.length; r +=1){
+        for(let c = 0; c < grid[0].length; c +=1){
+            let size = exploreSize(grid, r, c, visited);
+            // console.log(size)
+            if(size > 0 && size < minSize){
+                minSize = size;
+            }
+        }
+    }
+    return minSize;
+}
+
+const exploreSize = (grid, r, c, visited) => {
+    let rowInbounds = 0 <= r && r < grid.length;
+    let colInbounds = 0 <= c && c < grid[0].length;
+    if(!rowInbounds || !colInbounds) return 0;
+    
+    if(grid[r][c] === 'W') return 0;
+
+    let pos = r + "," + c;
+    if(visited.has(pos)) return 0;
+    visited.add(pos)
+
+    let size = 1;
+    size += exploreSize(grid, r-1, c, visited);
+    size += exploreSize(grid, r+1, c, visited);
+    size += exploreSize(grid, r, c-1, visited);
+    size += exploreSize(grid, r, c+1, visited);
+    return size;
+}
+
+console.log(minimumIsland(grid))
